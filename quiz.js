078afd4bbe3851472e53eb585ab1f5eb98c92eff
Ganbayar <truelo_0vee@yahoo.com>
2019-12-21 21:08:08 -0800

    added timer

--- quiz.js
+++ quiz.js
@@ -19,3 +19,26 @@ Quiz.prototype.guess = function(answer) {
 Quiz.prototype.isEnded = function() {
     return this.questionIndex === this.questions.length;
 }
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var oneMinutes = 75 * 1,
+        display = document.querySelector('#timer');
+    startTimer(oneMinutes, display);
+};
